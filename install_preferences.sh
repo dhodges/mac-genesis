1fc1d700a2207ae7144896c84c50bdc64421ff21
david hodges <hodges.david@gmail.com>
2014-10-31 12:57:16 +0000

    tweak prefs

--- install_preferences.sh
+++ install_preferences.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -e
 
-cd ~/build/dhodges_dotfiles
+cd prefs
 
 cp com.googlecode.iterm2.plist ~/Library/Preferences
-
-ln -s '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl' ~/bin/subl
 cp sublime_text/* "~/Library/Application Support/Sublime Text 2/Packages/User"
 
+ln -s '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl' ~/bin/subl
