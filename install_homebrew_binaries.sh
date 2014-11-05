44240b27f2861303d2856dc70aafeca4aef53034
david hodges <hodges.david@gmail.com>
2014-11-05 09:23:45 +0000

    tweak

--- install_homebrew_binaries.sh
+++ install_homebrew_binaries.sh
@@ -9,12 +9,14 @@ binaries=(
   elasticsearch090
   git
   postgresql
-  postgis20
+  postgis
   rbenv
   rbenv-bundler
   ruby-build
   redis
   tmux
+  watch
+  wget
 )
 
 brew tap homebrew/versions
@@ -32,11 +34,18 @@ brew linkapps
 echo "cleaning up homebrew..."
 brew cleanup
 
+# launch postgresql on login
+echo "Linking postgresql, to launch on login:"
+ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
+
+echo
+echo "Remember to install XCode and run `xcode-select --install'"
+echo "to install the command-line dev tools."
+echo
+
 # apple install of java for yosemite
 # http://support.apple.com/kb/DL1572
 
-# launch postgresql on login
-ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-
-# Remember to install XCode and run `xcode-select --install`
-# to install the command-line dev tools
+#psql template1 -f /usr/local/share/postgis/postgis.sql
+#psql template1 -f /usr/local/share/postgis/.sql
+#psql template1 -f /usr/local/share/postgis/postgis.sql
