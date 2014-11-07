211e9b469222c371d11e50fd9b54fc46737e0d0e
david hodges <hodges.david@gmail.com>
2014-11-07 08:34:23 +0000

    add Terminal prefs, and tweak prefs script

--- install_preferences.sh
+++ install_preferences.sh
@@ -1,21 +1,26 @@
-#!/bin/sh -xe
+#!/bin/sh -e
 
-cd prefs
-
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE_DIR=$THIS_DIR/prefs
 PREFS_DIR=~/Library/Preferences
 
-# iterm doesn't respect symlinks (grrr)
-cp com.googlecode.iterm2.plist $PREFS_DIR/com.googlecode.iterm2.plist
+SUBL_USER_DIR="$HOME/Library/Application Support/Sublime Text 2/Packages/User"
 
-[[ -f $PREFS_DIR/org.shiftitapp.ShiftIt.plist ]]                    || ln -sfv `pwd`/org.shiftitapp.ShiftIt.plist $PREFS_DIR
-[[ -f $PREFS_DIR/com.runningwithcrayons.Alfred-Preferences.plist ]] || ln -sfv `pwd`/com.runningwithcrayons.Alfred-Preferences.plist $PREFS_DIR
-[[ -f $PREFS_DIR/com.runningwithcrayons.alfred-2.plist ]]           || ln -sfv `pwd`/com.runningwithcrayons.alfred-2.plist $PREFS_DIR
+
+# neither Terminal nor iTerm respect symlinks (grrr)
+cp $SOURCE_DIR/com.googlecode.iterm2.plist $PREFS_DIR
+cp $SOURCE_DIR/com.apple.Terminal.plist    $PREFS_DIR
+
+cd $PREFS_DIR
+
+[[ -f com.runningwithcrayons.Alfred-Preferences.plist ]] || ln -sfv $SOURCE_DIR/com.runningwithcrayons.Alfred-Preferences.plist
+[[ -f com.runningwithcrayons.alfred-2.plist ]]           || ln -sfv $SOURCE_DIR/com.runningwithcrayons.alfred-2.plist
+[[ -f org.shiftitapp.ShiftIt.plist ]]                    || ln -sfv $SOURCE_DIR/org.shiftitapp.ShiftIt.plist
 
 [[ -e ~/bin/subl ]] || ln -s '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl' ~/bin/subl
 
-SUBL_USER_DIR="$HOME/Library/Application Support/Sublime Text 2/Packages/User"
-mkdir -p          "$SUBL_USER_DIR"
-cp sublime_text/* "$SUBL_USER_DIR"
+mkdir -p "$SUBL_USER_DIR"
+cp $SOURCE_DIR/sublime_text/* "$SUBL_USER_DIR"
 
 
 # Sublime Text 2 Package Manager installation:
