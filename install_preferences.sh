81d75ee83ab0a01499071575cf0b8f8f04ecd1fa
david hodges <david@tonga.local>
2014-11-09 12:00:49 +0000

    tweaked dbvisualiser prefs

--- install_preferences.sh
+++ install_preferences.sh
@@ -17,6 +17,10 @@ cd $PREFS_DIR
 [[ -f com.runningwithcrayons.alfred-2.plist ]]           || ln -sfv $SOURCE_DIR/com.runningwithcrayons.alfred-2.plist
 [[ -f org.shiftitapp.ShiftIt.plist ]]                    || ln -sfv $SOURCE_DIR/org.shiftitapp.ShiftIt.plist
 
+cd $HOME
+
+[[ -d ~/.dbvis ]]   || ln -sfv $SOURCE_DIR/_dbvis .dbvis
+
 [[ -e ~/bin/subl ]] || ln -s '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl' ~/bin/subl
 
 mkdir -p "$SUBL_USER_DIR"
