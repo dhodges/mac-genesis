5310dca3abab1e3a5db45bfa22b075ac335e7ecc
david hodges <david.hodges@lonelyplanet.com.au>
2014-11-18 14:57:42 +0000

    centralised homebrew install

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -2,16 +2,133 @@
 
 # see: http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac
 
-# Check for Homebrew,
-# Install if we don't have it
-
 if test ! $(which brew); then
-  echo "Installing homebrew..."
+  echo "installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Update homebrew recipes
+echo "updating homebrew..."
 brew update
 
-# Remember to install XCode and run `xcode-select --install`
-# to install command-line dev tools
+echo "tapping homebrew/versions..."
+brew tap homebrew/versions
+
+binaries=(
+  ack
+  bash-completion
+  cheat
+  elasticsearch090
+  git
+  hub
+  node
+  postgresql
+  postgis
+  pstree
+  rbenv
+  ruby-build
+  rbenv-bundler
+  redis
+  tmux
+  watch
+  wget
+)
+
+echo "installing homebrew binaries..."
+brew install ${binaries[@]}
+
+echo "installing (homebrew) emacs..."
+brew install emacs --cocoa --srgb
+
+brew linkapps
+
+echo
+echo "Linking postgresql to launch on login:"
+ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
+
+echo
+echo "Linking emacs to launch on login:"
+ln -sfv /usr/local/opt/emacs/*.plist ~/Library/LaunchAgents
+
+# apple install of java for yosemite
+# http://support.apple.com/kb/DL1572
+
+# oracle installation of jdk v7:
+# http://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html
+
+echo
+echo "installing GNU core utilities..."
+
+core_binaries=(
+    coreutils
+    findutils # find, locate, updatedb, xargs
+    bash      # bash v4
+)
+
+brew install ${core_binaries[@]}
+
+# Install more recent versions of some OS X tools
+echo
+echo "tapping homebrew/dupes..."
+brew tap homebrew/dupes
+
+echo
+echo "installing updated grep..."
+brew install homebrew/dupes/grep
+
+
+echo "installing homebrew cask..."
+brew install caskroom/cask/brew-cask
+
+echo "updating homebrew cask..."
+brew cask update
+
+apps=(
+    alfred
+    google-chrome
+    firefox
+    iterm2
+    sublime-text
+    atom
+    dbvisualizer
+    shiftit
+    xquartz
+    sourcetree
+    screenhero
+    #dropbox
+    #google-drive
+)
+
+# NB: dbvisualiser only provides an installer
+# which still needs to be run (manually).
+# Typically placed somewhere like:
+#
+# `/opt/homebrew-cask/Caskroom/dbvisualizer/9.1.11/DbVisualizer Installer.app`
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo
+echo "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
+
+# reveal these cask app links to Alfred
+brew cask alfred link
+
+echo
+echo "tapping caskroom/fonts..."
+brew tap caskroom/fonts
+
+fonts=(
+    font-inconsolata
+)
+
+echo "installing fonts..."
+brew cask install ${fonts[@]}
+
+echo
+echo "cleaning up homebrew..."
+brew cleanup
+
+echo
+echo "Remember to install XCode and run 'xcode-select --install'"
+echo "to install the command-line dev tools."
+echo
---
+++ install_rubies.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# see: http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac
+
+echo "installing ruby 1.9.3..."
+rbenv install 1.9.3-p392
+echo
+
+echo "installing ruby 2.0.0..."
+rbenv install 2.0.0-p353
+echo
+
+echo "installing ruby 2.1.1..."
+rbenv install 2.1.1
+echo
