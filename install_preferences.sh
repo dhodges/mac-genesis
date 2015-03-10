a002f2259c77bb391db75577b80ac6dfdef62be0
david hodges <david.hodges@lonelyplanet.com.au>
2015-03-10 11:20:58 +0000

    update prefs

--- install_preferences.sh
+++ install_preferences.sh
@@ -3,9 +3,10 @@
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PREFS_DIR=$THIS_DIR/prefs
 
-# neither Terminal nor iTerm respect symlinks (grrr)
+# neither Terminal, nor iTerm, nor DbVisualizer respect symlinks (grrr)
 cp $PREFS_DIR/com.googlecode.iterm2.plist ~/Library/Preferences
 cp $PREFS_DIR/com.apple.Terminal.plist    ~/Library/Preferences
+[[ -d ~/.dbvis ]] || cp -rf $PREFS_DIR/_dbvis ~/.dbvis
 
 files=(
     com.runningwithcrayons.Alfred-Preferences.plist
@@ -17,10 +18,10 @@ for file in ${files[@]}; do
     [[ -f ~/Library/Preferences/$file ]] || ln -sfv $PREFS_DIR/$file
 done
 
-[[ -d ~/.dbvis ]]   || ln -sfv $PREFS_DIR/_dbvis ~/.dbvis
 
 [[ -e ~/bin/subl ]] || ln -s '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl' ~/bin/subl
 
+# not much symlink love from sublimeT either (double-grrr)
 SUBL_USER_DIR="$HOME/Library/Application Support/Sublime Text 2/Packages/User"
 mkdir -p "$SUBL_USER_DIR"
 cp $PREFS_DIR/sublime_text/* "$SUBL_USER_DIR"
