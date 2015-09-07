d342a16386bafa13b937ab23898346b48439bba8
david hodges <david.hodges@lonelyplanet.com>
2015-09-07 11:59:46 +0100

    conflate repo(s) installation

--- install_repos.sh
+++ install_repos.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/sh -e
+
 
 mkdir -p ~/build
 cd ~/build
@@ -6,3 +7,27 @@ cd ~/build
 [[ -e dotfiles  ]] || git clone git@github.com:dhodges/dotfiles.git dhodges_dotfiles
 [[ -e solarized ]] || git clone git://github.com/altercation/solarized.git
 
+cd dhodges_dotfiles
+./bin/setupdotfiles
+
+
+mkdir -p ~/projects
+cd ~/projects
+
+repos=(
+    atlas
+    chef-repo
+    cloudformation
+    geo
+    lpos-chef-repo
+    map_tile_loader
+    pairing_station
+    remixer
+    spp_aws
+    spp_boxen
+    spp_dashboard
+)
+
+for repo in ${repos[@]}; do
+    [[ -e ~/projects/$repo ]] || echo '' && git clone git@github.com:lonelyplanet/$repo.git
+done
