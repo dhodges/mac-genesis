39f65b0e1bd989c72b7b20336c31037345da4124
dhodges <hodges.david@gmail.com>
2019-08-16 11:19:11 +1000

    fix clone of dotfiles

--- 3.install_repos.sh
+++ 3.install_repos.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/build
 
 [[ -e ~/org ]]             || git clone git@github.com:dhodges/org-mode-files.git    ~/org
-[[ -e ~/build/dotfiles  ]] || git clone git@github.com:dhodges/dotfiles.git dotfiles ~/build/dotfiles
+[[ -e ~/build/dotfiles  ]] || git clone git@github.com:dhodges/dotfiles.git          ~/build/dotfiles
 [[ -e ~/build/solarized ]] || git clone git://github.com/altercation/solarized.git   ~/build/solarized
 
 cd ~/build/dotfiles
