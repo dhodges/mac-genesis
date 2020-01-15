f5eaa328d8e6ee4961aa53aa35f6e508b35d1224
David Hodges <hodges.david@gmail.com>
2020-01-15 16:40:47 +1100

    remove typo

--- 4.install_preferences.sh
+++ 4.install_preferences.sh
@@ -8,7 +8,7 @@ echo "symlinking preference files..."
 # neither Terminal nor iTerm respect symlinked pref files (grrr)
 cp $PREFS_DIR/com.googlecode.iterm2.plist ~/Library/Preferences
 cp $PREFS_DIR/com.apple.Terminal.plist    ~/Library/Preferences
-w
+
 [[ -f ~/Library/Preferences/org.shiftitapp.ShiftIt.plist ]] || ln -sfv $PREFS_DIR/$file
 
 echo "VSCode extensions"
