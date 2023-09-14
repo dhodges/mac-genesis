7f6401d112dd67aee85fe06cc006d88fcd174f71
David Hodges <dhodges@zendesk.com>
2023-09-14 18:39:04 +1000

    update 3.install_preferences.sh

--- 3.install_preferences.sh
+++ 3.install_preferences.sh
@@ -8,6 +8,7 @@ echo "symlinking preference files..."
 # neither Terminal nor iTerm respect symlinked pref files (grrr)
 cp $PREFS_DIR/com.googlecode.iterm2.plist ~/Library/Preferences
 cp $PREFS_DIR/com.apple.Terminal.plist    ~/Library/Preferences
+cp $PREFS_DIR/vscode_user_settings.json  "~/Library/Application Support/Code/User/settings.json"
 
 [[ -f ~/Library/Preferences/org.shiftitapp.ShiftIt.plist ]] || ln -sfv $PREFS_DIR/$file
 
